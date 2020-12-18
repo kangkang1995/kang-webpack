5128e99224167a092377d6363217801a0f7e3268
chenhuaxiang <chenhuaxiang@123.com>
2020-12-18 10:21:01 +0800

    增加 文档描述

---
+++ src/modules/page2/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { render } from "react-dom";
+const root = document.getElementById("app");
+const load = () =>
+    render(
+        <>
+            <div>
+                1111111111
+            </div>
+        </>,
+        root
+    );
+load();
